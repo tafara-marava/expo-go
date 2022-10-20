948f83cab3d81937ce2ad2a7dda2145ccfd72769
Tafara Marava <tafara@Tafaras-MacBook-Pro.local>
2022-10-20 13:52:55 -0600

    added input and button

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
+import { Button, TextInput } from 'react-native-paper';
 
 
 
@@ -13,6 +14,7 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [isLoggedIn,setIsLoggedIn] = React.useState(false); // edit in this line 
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
 
    if (isFirstLaunch == true){
@@ -20,9 +22,47 @@ return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else{
+  }else if(isLoggedIn){
     return <Navigation/>
-  }
+  }return (
+    <view>
+      <TextInput
+        style= {styles.input}
+        placeholderTextColor="#4251fs"
+        placeholder='Cell Phone'>
+
+      </TextInput>
+      <Button
+        title='Send'
+        style={styles.button}
+        onPress={()=>{
+          console.log('Button was pressed')
+        }}/> 
+    </view>
+  )
 }
  export default App;
+
+ const styles = StyleSheet.create({
+  container:{
+      flex:1, 
+      alignItems:'center',
+      justifyContent: 'center'
+  },
+  input: {
+    marginTop: 100,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  margin:{
+    marginTop:100,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }    
+})
 
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 import { SafeAreaView } from 'react-navigation';
 
@@ -44,7 +44,7 @@ const OnboardingScreen = ({setFirstLaunch}) =>{
               backgroundColor: '#fff',
               image: <Image   style={{height: '60%', width: '90%', resizeMode:'contain', marginBottom:-200}} source={require('../image/testing.png')} />,
               title: <Text style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: -70}}>Welcome to STEDI  Balance</Text>,
-              subtitle: 'STEDI Balance will now send a text with a code',
+              subtitle:'Welcome to STEDI'
             },
             {
                 backgroundColor: '#fff',
